44b600e8be88ac97a3ce621d18390f26bbaf6ddd
Alan <alanstvn420@gmail.com>
2023-08-29 09:41:38 -0500

    Alan

--- src/Cajero.java
+++ src/Cajero.java
@@ -3,6 +3,17 @@ import javax.swing.*;
 public class Cajero {
     public JPanel CajeroP;
     private JLabel LCajero;
+    private JTextField TNOMBRE;
+    private JTextField TAPELLIDO;
+    private JTextField TCORREO;
+    private JTextField TTELEF;
+    private JTextField TCEDULA;
+    private JTextField TCOD;
+    private JTextField TCANTIDAD;
+    private JLabel TPROD;
+    private JLabel TPRECIO;
+    private JLabel TSUBT;
+    private JLabel TTOTAL;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Cajero");
