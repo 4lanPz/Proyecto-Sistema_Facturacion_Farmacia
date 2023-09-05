a44c815ec5de255f7725cb7d55169d4feccb54ef
Alan <alanstvn420@gmail.com>
2023-09-05 12:59:12 -0500

    FACTURA FUNCIONAL y sql

--- src/Factura.java
+++ src/Factura.java
@@ -110,3 +110,71 @@ public class Factura {
         frame.setVisible(true);
     }
 }
+
+/*
+* DROP DATABASE PROYECTO2023A;
+Create database PROYECTO2023A;
+use PROYECTO2023A;
+
+Create Table Admin(
+Correo varchar(25),
+Nombre varchar(15),
+Apellido varchar(15),
+Contrasenia varchar(30));
+
+Create Table Cajero(
+ID int primary key,
+Nombre varchar(15),
+Apellido varchar(15),
+Correo varchar(25),
+Contrasenia varchar(30));
+
+Create Table Producto(
+Cod int primary key,
+Nom varchar(35),
+Precio double,
+Stock int);
+
+Create Table Cliente(
+Cedula int primary key,
+Nombrecli varchar(35),
+Apelcli varchar(35),
+Correocli varchar(35),
+Telef int);
+
+Create Table Factura(
+numfac int,
+cantidad int,
+IDCaj int,
+CedulaCli int,
+Codprod int,
+FOREIGN KEY (Codprod) REFERENCES Producto(Cod),
+FOREIGN KEY (CedulaCli) REFERENCES Cliente(Cedula),
+FOREIGN KEY (IDCaj) REFERENCES Cajero(ID));
+
+insert into cliente
+values(12345,'nomcli1','apelcli1','correocli1',123456789);
+insert into producto
+values(54321,'prod1',1.20,100);
+insert into producto
+values(1,'prod1',1.20,100),
+(2,'prod2',1.30,100),
+(3,'prod3',1.50,100),
+(4,'prod4',1.80,100),
+(5,'prod5',2.90,100);
+insert into cajero
+values(123,'nomcaj','apelcaj','correocaj','contra');
+
+insert into cajero
+values(12345,'nomcaj','apelcaj','correocaj','contra');
+Insert into Factura
+values(1,123,123,12345,54321);
+
+select * from cajero
+
+SELECT * FROM FACTURA
+WHERE IDCAJ = 123 and numfac = 1000
+
+select Nombrecli CLiente from Cliente A, Factura b
+where Cedula = CedulaCli and Numfac = 1000
+* */
