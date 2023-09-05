b9454337104472a34c3faeab391473d00a5409d2
Alan <alanstvn420@gmail.com>
2023-09-05 11:43:58 -0500

    FACTURA FUNCIONAL

--- src/Cajero.java
+++ src/Cajero.java
@@ -35,6 +35,7 @@ public class Cajero {
     private JTextField TCANTIDAD5;
     private JButton FACTURAButton;
     private JTextField TNumFac;
+    private JButton BVISTA;
     int contador = 0;
     //cliente
     String Nom,Apel,Mail,Ced,Telef;
@@ -44,10 +45,6 @@ public class Cajero {
     Double PreciosP [] = {0.0,0.0,0.0,0.0,0.0};
     int CantidadesP[] = {0,0,0,0,0};
     Double Subtotales [] = {0.0,0.0,0.0,0.0,0.0};
-    //
-    String NombrePR;
-    Double PrecioPR;
-
     //Total Factura
     double Subtotal, Valoriva= 0.12 ,Total;
     int NumFac;
@@ -57,7 +54,7 @@ public class Cajero {
     static String PASS="root_bas3";
 
     public void CODIGOPRODUCTO(){
-                    for (int i = 0; i <= 4 ; i++){
+        for (int i = 0; i <= 4 ; i++){
             String SELECT_QUERY="SELECT * FROM Producto WHERE COD = ?";
             //try(Connection conn=DriverManager.getConnection(DB_URL,USER,PASS);)
             try(Connection conn=DriverManager.getConnection(DB_URL,USER,PASS);)
@@ -74,12 +71,21 @@ public class Cajero {
                 throw new RuntimeException(eX);
             }
         }
+        TPROD1.setText(NombresP[0]);
+        TPROD2.setText(NombresP[1]);
+        TPROD3.setText(NombresP[2]);
+        TPROD4.setText(NombresP[3]);
+        TPROD5.setText(NombresP[4]);
+        TPRECIO1.setText(String.valueOf(PreciosP[0]));
+        TPRECIO2.setText(String.valueOf(PreciosP[1]));
+        TPRECIO3.setText(String.valueOf(PreciosP[2]));
+        TPRECIO4.setText(String.valueOf(PreciosP[3]));
+        TPRECIO5.setText(String.valueOf(PreciosP[4]));
     }
 
     public void CALCULOS(){
         for (int i = 0; i <= 4 ; i++){
             String SELECT_QUERY="SELECT * FROM Producto WHERE COD = ?";
-            //try(Connection conn=DriverManager.getConnection(DB_URL,USER,PASS);)
             try(Connection conn=DriverManager.getConnection(DB_URL,USER,PASS);)
             {
                 PreparedStatement statement = conn.prepareStatement(SELECT_QUERY);
@@ -94,10 +100,34 @@ public class Cajero {
             }
         }
     }
+
     public Cajero(){
         FACTURAButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Ingresar los valores de las facturas
+                for (int i = 0; i <= 4 ; i++){
+                    String Ingresar_FAC ="INSERT INTO Factura VALUES(?,?,?,?,?)";
+                    try(Connection conn=DriverManager.getConnection(DB_URL,USER,PASS);)
+                    {
+                        PreparedStatement statement = conn.prepareStatement(Ingresar_FAC);
+                        statement.setInt(1, NumFac);
+                        statement.setInt(2, CantidadesP[i]);
+                        statement.setInt(3, 123);
+                        statement.setInt(4, 123);
+                        statement.setInt(5, CodigosP[i]);
+                        statement.executeUpdate();
+                    }
+                    catch (SQLException eX){
+                        throw new RuntimeException(eX);
+                    }
+                }
+            }
+        });
+        BVISTA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NumFac = Integer.parseInt(TNumFac.getText());
                 //cliente
                 Ced = TCEDULA.getText();
                 Nom = TNOMBRE.getText();
@@ -111,6 +141,7 @@ public class Cajero {
                 CodigosP[3] = Integer.parseInt(TCOD4.getText());
                 CodigosP[4] = Integer.parseInt(TCOD5.getText());
                 CODIGOPRODUCTO();
+
                 //CANTIDADES
                 CantidadesP[0] = Integer.parseInt(TCANTIDAD1.getText());
                 CantidadesP[1] = Integer.parseInt(TCANTIDAD2.getText());
@@ -123,33 +154,28 @@ public class Cajero {
                     Subtotales[i]=PreciosP[i]*CantidadesP[i];
                     Subtotal += Subtotales[i];
                 }
+                TSUBT.setText(String.valueOf(Subtotal));
                 Total=Subtotal+(Subtotal*Valoriva);
                 TTOTAL.setText(String.valueOf(Total));
-
-                //Ingreso valores a base y factura
+                //Ingres valores del cliente
                 String Q_Ingresar ="INSERT INTO CLIENTE VALUES(?,?,?,?,?)";
                 try(Connection conn=DriverManager.getConnection(DB_URL,USER,PASS);)
                 {
                     PreparedStatement statement = conn.prepareStatement(Q_Ingresar);
-                    // Establecer valores para los parámetros de la sentencia SQL
                     statement.setInt(1, Integer.parseInt(Ced));
                     statement.setString(2, Nom);
                     statement.setString(3, Apel);
                     statement.setString(4, Mail);
                     statement.setString(5, Telef);
-                    } catch (SQLException ex) {
+                    statement.executeUpdate();
+                } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
-                //Ingresar los valores de las facturas
-                NumFac = Integer.parseInt(TNumFac.getText());
-
             }
         });
-
     }
 
     public void setVisible(boolean b) {
-
     }
 
     public static void main(String[] args) {
